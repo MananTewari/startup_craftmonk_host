52c722508115b05641bbd2be6e5669ba541237a9
MananTewari <85873623+MananTewari@users.noreply.github.com>
2024-03-30 23:23:15 +0530

    shopping cart rendered

---
+++ production_craftmonk/src/Components/Emptycart.jsx
@@ -0,0 +1,9 @@
+function Emptycart() {
+  return (
+    <div>
+      <h1>Clean</h1>
+    </div>
+  );
+}
+
+export default Emptycart;
--- production_craftmonk/src/routes/ShoppingBag.jsx
+++ production_craftmonk/src/routes/ShoppingBag.jsx
@@ -1,5 +1,28 @@
-function ShoppingBag() {
-    return ( <div>Shopping</div>  );
-}
+import { useSelector } from "react-redux";
+import BagItems from "../Components/bagItems";
+import BagSummary from "../Components/summary";
+import Emptycart from "../Components/Emptycart";
 
+function ShoppingBag() {
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => bagItems.includes(item.id));
+  const length = bagItems.length;
+  return (
+    <>
+      {bagItems.length === 0 ? (
+       <Emptycart/>
+      ) : (
+        <div className="bag-page">
+          <div className="bag-item-container">
+            {finalItems.map((item) => (
+              <BagItems key={item.id} item={item} />
+            ))}
+          </div>
+          <BagSummary />
+        </div>
+      )}
+    </>
+  );
+}
 export default ShoppingBag;
