632309fde1090817b54b83ca115c3ffc74a7e7ff
MananTewari <85873623+MananTewari@users.noreply.github.com>
2024-04-09 12:05:10 +0530

    grid corrected

--- production_craftmonk/src/Components/createItems.jsx
+++ production_craftmonk/src/Components/createItems.jsx
@@ -16,19 +16,27 @@ function ItemsCreater({ item }) {
   };
 
   return (
-    <div className="col-md-3 col-sm-6 mb-4"> {/* Add mb-4 for bottom margin */}
-      <div className="card h-100">
-        <img src={item.image} className="card-img-top" alt="item image" />
-        <div className="card-body">
-          <h5 className="card-title">{item.item_name}</h5>
-          <p className="card-text">{item.company}</p>
-          <p className="card-text">
+    <div className="col-md-3 col-sm-6 mb-4">
+      <div className="product-tile">
+        <div className="product-image-container">
+          <img
+            src={item.image}
+            className="product-image"
+            alt="item image"
+          />
+        </div>
+        <div className="product-details">
+          <h5 className="product-title">{item.item_name}</h5>
+          <p className="product-company">{item.company}</p>
+          <p className="product-price">
             <span className="current-price">Rs {item.current_price}</span>
             <span className="original-price">Rs {item.original_price}</span>
-            <span className="discount">({item.discount_percentage}% OFF)</span>
+            <span className="discount">
+              ({item.discount_percentage}% OFF)
+            </span>
           </p>
         </div>
-        <div className="card-footer">
+        <div className="product-overlay">
           {elementFound ? (
             <button className="btn btn-danger" onClick={handleBagRemove}>
               Delete From Bag
--- production_craftmonk/src/Components/summary.jsx
+++ production_craftmonk/src/Components/summary.jsx
@@ -4,8 +4,7 @@ import Checkout from "./Checkout";
 import useDispatch from "react-redux";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 
 const BagSummary = () => {
   
